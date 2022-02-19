972ef2227441b4ad075258210cff3ac4b8443739
VagnerCastro <vagnercastroreis.gmail.com>
2022-02-18 21:50:07 -0300

    feature-add Tag e Text (js/css)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Tag from "./Components/Tag/Tag";
+import Text from "./Components/Text/Text";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Tag ContainerCard='todo'></Tag>
+        <Tag ContainerCard='inprogress'></Tag>
+        <Tag ContainerCard='review'></Tag>
+        <Tag ContainerCard='done'></Tag>
+        <Text textContent="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. ">seu texto</Text>
+
     </div>
   );
 }
