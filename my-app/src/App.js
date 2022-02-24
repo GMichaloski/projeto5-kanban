e8fbf4ad103251f1555a9670032cbd3ef4874712
GMichaloski <gustavomichaloski@gmail.com>
2022-02-23 21:35:24 -0300

    Css do background e dos cards praticamente prontos

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,73 +12,74 @@ function App() {
   return (
     <div>
       <body className="App">
-          <header className="App-header">
-            <div>
-              <h1>Kanban</h1>
-            </div>
-          </header>
-            
-          <main className='App App-Container'>
-              <div className='App-Container Todo'>
-                  <ExternCard cardType="todo" title="🗒️ To Do">
-                    <InternCard img={male} title="Christmas Banners">
-                      <Tag ContainerCard="todo" txt="Label"></Tag>
-                    </InternCard>
-                    <InternCard img={male} title="Redo Portfolio">
-                      <Tag ContainerCard="todo" txt="Label"></Tag>
-                    </InternCard>
-                  </ExternCard>
-              </div>
+        <div className="background-color"></div>
+        <header className="App-header">
+          <div>
+            <h1>Kanban</h1>
+          </div>
+        </header>
 
-            <div class="App App-Inprogress">
-              <ExternCard cardType="inprogress" title="💻 In Progress">
-                <InternCard img={female} title="Coffee Break">
-                  <Tag ContainerCard="inprogress" txt="Always"></Tag>
-                </InternCard>
-                <InternCard img={female} title="Updating Portfolio">
-                  <Tag ContainerCard="inprogress" txt="Webflow"></Tag>
-                </InternCard>
-              </ExternCard>
+        <main className="App App-Container">
+          <div className="App Todo">
+            <ExternCard cardType="todo" title="🗒️ To Do">
+              <InternCard img={male} title="Christmas Banners">
+                <Tag ContainerCard="todo" txt="Label"></Tag>
+              </InternCard>
+              <InternCard img={male} title="Redo Portfolio">
+                <Tag ContainerCard="todo" txt="Label"></Tag>
+              </InternCard>
+            </ExternCard>
+          </div>
 
-            </div>
+          <div class="App App-Inprogress">
+            <ExternCard cardType="inprogress" title="💻 In Progress">
+              <InternCard img={female} title="Coffee Break">
+                <Tag ContainerCard="inprogress" txt="Always"></Tag>
+              </InternCard>
+              <InternCard img={female} title="Updating Portfolio">
+                <Tag ContainerCard="inprogress" txt="Webflow"></Tag>
+              </InternCard>
+            </ExternCard>
+          </div>
 
+          <div class="App App-review">
+            <ExternCard cardType="review" title="🖍 Review">
+              <InternCard img={male} title="Release to Figma Community">
+                <Tag ContainerCard="review" txt="Release"></Tag>
+              </InternCard>
+              <InternCard img={male} title="User Feedback">
+                <Tag ContainerCard="review" txt="Feedback"></Tag>
+              </InternCard>
+              <InternCard
+                img={male}
+                title="Background images from humaaans.com"
+              >
+                <Tag ContainerCard="review" txt="Sourcing"></Tag>
+              </InternCard>
+            </ExternCard>
+          </div>
 
-              <div class="App App-review">
-              <ExternCard cardType="review" title="🖍 Review">
-                <InternCard img={male} title="Release to Figma Community">
-                  <Tag ContainerCard="review" txt="Release"></Tag>
-                </InternCard>
-                <InternCard img={male} title="User Feedback">
-                  <Tag ContainerCard="review" txt="Feedback"></Tag>
-                </InternCard>
-                <InternCard img={male} title="Background images from humaaans.com">
-                  <Tag ContainerCard="review" txt="Sourcing"></Tag>
-                </InternCard>
-              </ExternCard>
-            </div>
-
-            <div class="App App-done" >
-              <ExternCard cardType="done" title="🚀 Done">
-                <InternCard img={female} title="Style Guide">
-                  <Tag ContainerCard="done" txt="UI"></Tag>
-                </InternCard>
-                <InternCard img={female} title="Component Library">
-                  <Tag ContainerCard="done" txt="UI"></Tag>
-                </InternCard>
-                <InternCard img={female} title="Sticker Components">
-                  <Tag ContainerCard="done" txt="UI"></Tag>
-                </InternCard>
-              </ExternCard>
-
-            </div>
-      
-          </main>
-          <footer>
-              <div>
-                  <img src={background} className="App-background" alt="background"></img>
-              </div>
-
-          </footer>
+          <div class="App App-done">
+            <ExternCard cardType="done" title="🚀 Done">
+              <InternCard img={female} title="Style Guide">
+                <Tag ContainerCard="done" txt="UI"></Tag>
+              </InternCard>
+              <InternCard img={female} title="Component Library">
+                <Tag ContainerCard="done" txt="UI"></Tag>
+              </InternCard>
+              <InternCard img={female} title="Sticker Components">
+                <Tag ContainerCard="done" txt="UI"></Tag>
+              </InternCard>
+            </ExternCard>
+          </div>
+        </main>
+        <div>
+          <img
+            src={background}
+            className="App-background"
+            alt="background"
+          ></img>
+        </div>
       </body>
     </div>
   );
